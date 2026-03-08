3672454ce8fd34bbe66abd56f5b02489c3ed5d5e
novabuster0 <novabuster2005@gmail.com>
2026-03-08 21:35:25 +0200

    ready

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [tailwindcss(), react()],
+    build: {
+        outDir: "dist",
+        sourcemap: false,
+        minify: "terser",
+        chunkSizeWarningLimit: 1000,
+    },
+    server: {
+        port: 3000,
+        open: true,
+    },
 });
